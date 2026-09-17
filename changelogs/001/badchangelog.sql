925b13f483d50f5524a90d15f042b682de85336b
Rick Broker <rbroker@liquibase.com>
2026-09-17 11:25:39 -0400

    clean up

--- changelogs/001/badchangelog.sql
+++ changelogs/001/badchangelog.sql
@@ -1,6 +1,6 @@
 --liquibase formatted sql
 
---changeset jbennett:ddl_create_table_organizations labels:release-1.0.0
+--changeset rbroker:ddl_create_table_organizations labels:release-1.0.0
 --comment Release 1.0.0
 SELECT * FROM ORGANIZATIONS;
 
